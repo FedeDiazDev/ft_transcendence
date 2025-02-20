d986fcfcac161f6fdea1ff5a7cf82801cec6f4cd
fmoran-m <franmoranmato@gmail.com>
2025-02-20 18:20:17 +0100

    fix: removed node_modules and dist

--- frontend/dist/main.js
+++
@@ -1,5 +0,0 @@
-import { Navbar } from "./components/Navbar.js";
-document.addEventListener("DOMContentLoaded", () => {
-    const app = document.getElementById("app");
-    document.body.insertBefore(Navbar(), app);
-});
