20551adf520cce84eb77bf2e748ae8d7c8e3527c
Federico Jose Diaz Guanoluiza <federicojose2000@gmail.com>
2025-02-21 18:47:24 +0100

    feat: rooted cards

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,8 @@
 import { Navbar } from "./components/Navbar.js";
+import { Home } from "./pages/home.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const app = document.getElementById("app");
+  app?.appendChild(Home());
   document.body.insertBefore(Navbar(), app);
 });
