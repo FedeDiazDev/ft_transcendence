d6163d2480019a082029ac30e51f3c5d32f6ec4c
fmoran-m <franmoranmato@gmail.com>
2025-02-24 18:44:45 +0100

    feat: auth-service dir

---
+++ auth-service/server.js
@@ -0,0 +1,11 @@
+const fastify = require('fastify')({ logger:true })
+
+fastify.get('/signup', function handler (request, reply){
+	const {username, email, pass} = request.body;
+	if (username && email && pass)
+		reply.send({is: 'working'});
+	else
+		reply.send({not: 'going'});
+})
+
+fastify.listen({ port: 3000, host: '0.0.0.0',});
