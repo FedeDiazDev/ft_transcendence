bb9380128d9ff209c59cdccca7e87731afb4e7f8
fmoran-m <franmoranmato@gmail.com>
2025-02-26 20:14:11 +0100

    added post password control

--- auth-service/server.js
+++ auth-service/server.js
@@ -1,4 +1,5 @@
 import Fastify from 'fastify'
+
 const fastify = Fastify({
 	logger: true
   })
@@ -8,28 +9,6 @@ const connectOptions = {
     port: 3000
 }
 
-const opts = {
-  schema: { 
-    body: {
-      type: "object",
-      properties: {
-        nickname: { type: "string" },
-        email: { type: "string" },
-        password: { type: "string" }
-      },
-      required: ["nickname", "email", "password"]
-    },
-    response: {
-      200: {
-        type: "object",
-        properties: {
-          hello: { type: "string" }
-        }
-      }
-    }
-  }
-};
-
 function serverError(err) {
     if (err) {
         fastify.log.error(err);
@@ -37,12 +16,31 @@ function serverError(err) {
     }
 }
 
+const opts = {
+	schema: { 
+	  body: {
+		type: "object",
+		properties: {
+		  nickname: { type: "string" },
+		  email: { type: "string" },
+		  password: { type: "string" },
+		  confirmPassword: { type: "string" }
+		},
+		required: ["nickname", "email", "password", "confirmPassword"]
+	  }
+	}
+  };
+  
+
 fastify.get('/', async function handler (request, reply) {
 	return { root: 'works' }
 })
 
 fastify.post("/signup", opts, async function handler (request, reply) {
-	reply.send({"post" : "complete"});
+	if (request.body.password != request.body.confirmPassword)
+		reply.status(400).send({message : "Password does not match"},);
+	else
+		reply.send({message : "Successful request"});
 });
 
 fastify.listen(connectOptions, serverError);
