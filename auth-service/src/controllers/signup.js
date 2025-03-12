ef1a95df50026dd4176acaab1e922f13c5b8fe60
fmoran-m <franmoranmato@gmail.com>
2025-03-12 07:17:15 +0100

    refactor: signup card

--- auth-service/src/controllers/signup.js
+++ auth-service/src/controllers/signup.js
@@ -31,5 +31,5 @@ export default function postSignup(request, reply){
 
 		const query = db.prepare("INSERT INTO users (username, email, password, salt) VALUES (?, ?, ?, ?)");
 		query.run(request.body.username, request.body.email, passStruct.hash, passStruct.salt);
-		reply.status(200).send({message : "Registration complete"});
+		reply.status(200).send({statusCode : 200, message : "Registration complete"});
 }
