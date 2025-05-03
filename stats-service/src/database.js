ff41ab1d5c21650a676b09906ba850ae77db7cbf
Ignacio Castillo Franco <nateto@hotmail.com>
2025-05-03 23:10:52 +0200

    Feat/stats container branch (#86)
    * compose up works with stats Container
    
    * game class has now attributes id and date
    
    * game data written OK to stats db when game is finished
    
    * stats container upstream was not added to nginx congig

---
+++ stats-service/src/database.js
@@ -0,0 +1,30 @@
+import fp from "fastify-plugin";
+import Database from "better-sqlite3";
+
+function dbConnector(fastify) {
+
+    const dbFile = "/data/stats.db";
+    const db = new Database(dbFile, { verbose: console.log });
+
+	db.pragma('journal_mode = WAL');
+
+    db.exec(`
+      CREATE TABLE IF NOT EXISTS games (
+        gameid INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT,
+        winner_username TEXT NOT NULL,
+        winner_points INTEGER NOT NULL,
+        looser_username TEXT NOT NULL,
+        looser_points INTEGER NOT NULL,
+        game_date TEXT NOT NULL
+      );
+    `);//SQLite doesn't have a native DATE type
+ 
+    fastify.decorate("db", db);
+
+    fastify.addHook("onClose", (done) => {
+      db.close();
+      done();
+    });
+  }
+  
+  export default fp(dbConnector);
---
+++ stats-service/src/server.js
@@ -0,0 +1,41 @@
+import Fastify from 'fastify';
+import dbConnector from './database.js';
+import routes from './routes.js';
+import cors from '@fastify/cors';
+
+const opts = {
+	logger: true,
+}
+
+const fastify = Fastify(opts);
+
+fastify.setErrorHandler((error, request, reply) => {
+	fastify.log.error(error);    
+	reply.send(error);
+});
+
+const connectOptions = {
+    host: '0.0.0.0',
+    port: 3000
+}
+
+function serverError(err) {
+    if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+}
+
+async function startServer(){
+	try{
+		await fastify.register(cors, { origin: '*' });
+		await fastify.register(routes);
+		await fastify.register(dbConnector);
+		await fastify.listen(connectOptions, serverError);
+	}catch(error){
+		fastify.log.error(error);
+		process.exit(1);
+	}
+}
+
+startServer();
