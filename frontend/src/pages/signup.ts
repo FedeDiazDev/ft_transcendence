d222cd43b3c74aa1c9d9b878bd1250db70a137fa
Federico Jose Diaz Guanoluiza <fdiaz-gu@c1r2s5.42madrid.com>
2025-07-03 15:39:46 +0200

    feat: login changes

--- frontend/src/pages/signup.ts
+++ frontend/src/pages/signup.ts
@@ -6,8 +6,8 @@ export const Signup = () => {
 
     const logo = document.createElement("a");
     logo.href = "/";
-    logo.textContent = "🏓 Pong";
-    logo.className = "text-4xl font-bold mb-3";
+    // logo.textContent = "🏓 Pong";
+    // logo.className = "text-4xl font-bold mb-3";
 
     div.appendChild(logo);
     div.appendChild(SignupCard());
